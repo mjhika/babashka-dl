39430d7abdb17c7dd3db1fb15ac8ddda3cd9b026
mjhika <1zzt6ovh@mojica.anonaddy.com>
2023-04-25 00:23:36 -0400

    add option to choose a version number
    update README with usage and remove known issues

--- README.md
+++ README.md
@@ -6,27 +6,39 @@
 
 Installs [Babashka](https://github.com/babashka/babashka) on Windows.
 
-I liked that [Michiel Borkent](https://github.com/borkdude) had the [install sh](https://raw.githubusercontent.com/babashka/babashka/master/install) and wanted something similar for Windows. 
+I liked that [Michiel Borkent](https://github.com/borkdude) had an 
+[install sh](https://raw.githubusercontent.com/babashka/babashka/master/install). 
+I wanted something similar for Windows. 
+
+This isn't intended to replace using a tool like `scoop` but instead for systems
+where scoop is just more overhead than you want to manage. For that reason this 
+script installs to the end of `$env:PATH`
 
 
-## Use
+## Usage
 
 ```powershell
 # to install in 'C:\Program Files\Babashka'
 irm "https://raw.githubusercontent.com/mjhika/babashka-dl/main/install.ps1" | iex
 # or
 irm "https://bb-dl.mjhika.com" | iex
+# By default you must run with Administrator so that it's on the PATH
+# otherwise use the other method to install to a different directory
 
 # to install in your current directory
 iex "& {$(irm https://raw.githubusercontent.com/mjhika/babashka-dl/main/install.ps1)} -Dir ."
-# or
+# or to a specific directory
 iex "& {$(irm bb-dl.mjhika.com)} -Dir ."
+# or a specific version
+iex "& {$(irm bb-dl.mjhika.com)} -Version v1.3.176"
+# or both
+iex "& {$(irm bb-dl.mjhika.com)} -Version v1.3.176 -Dir ."
 ```
 
 
 ## Known Issues
 
 
-Right now I only have feature parity for install directory and download the latests version as that's my main use case. 
+~~Right now I only have feature parity for install directory and download the latests version as that's my main use case.~~
 
 ~~Also unfortunately, the script currently only supports PowerShell 7. I believe this is because `Expand-Archive` changed from version 5 to 7 somewhere. When trying to use on PS 5 `Expand-Archive` will throw an error because temp file that is created is not a proper zip archive?~~
--- install.ps1
+++ install.ps1
@@ -1,74 +1,99 @@
 param (
     [string]
-    $Dir
+    $Dir = "C:\Program Files\Babashka",
+    [string]
+    $Version = "latest"
 )
 
 $ProgressPreference = 'SilentlyContinue'
 
 function Test-Elevation {
-    [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")    
+    [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 }
 
-function Ensure-InstallDir {
-    $local:InstallDir = 'C:\Program Files\Babashka'
-    if (Test-Path -Path $InstallDir -PathType Container) {
-        Get-Item -Path $InstallDir
-    } else {
-        New-Item -Path $InstallDir -ItemType Directory
-    }
-}
-
-function Ensure-InstallDirOnPath {
-    $local:OriginalEnvPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-    $local:bbDir = 'C:\Program Files\Babashka\'
-
-    if ($OriginalEnvPath.Split(";") -contains $bbDir) {
-        Write-Output "Babashka is on `$ENV:PATH."
-    } else {
-        $local:DesiredEnvPath = "$OriginalEnvPath;$bbDir"
-
-        Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value  $DesiredEnvPath
-        $env:path="$env:path;C:\Program Files\Babashka"
-        Ensure-InstallDirOnPath
-    }
-}
-
-function DownloadLatestBabashka {
+function Ensure-BabashkaProgramFile {
     param (
-        [string]
+        [String]
         $Directory
     )
-    $local:Releases = Invoke-WebRequest -Uri 'https://api.github.com/repos/babashka/babashka/releases/latest'
-    $local:Assets = ($Releases.Content | ConvertFrom-Json).assets
-    $local:LatestDownloadUrl = ($Assets | Where-Object {$_.browser_download_url -match 'windows-amd64.zip$'}).browser_download_url
-    $local:tmpf = New-TemporaryFile
-    
-    Invoke-WebRequest -Uri $latestDownloadUrl -OutFile $tmpf
-    
-    # I'll have to go fix this later. Reads horribly in my opinion, but will inflate the archive regardless of PSVer
-    # The first if checks if you supplied a directory.
-    # The second if checks PS version
-    if ($Directory) {
-        if ($PSVersionTable.PSVersion.Major -lt 7) {
-            if (Test-Path -Path (Join-Path $Directory "bb.exe") -PathType Leaf) {Remove-Item (Join-Path $Directory "bb.exe")}
-            [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null #redirect out to silence import
-            [System.IO.Compression.ZipFile]::ExtractToDirectory($tmpf, (Resolve-Path $Directory).Path)
-        } else {
-            Expand-Archive -Path $tmpf -DestinationPath $Directory -Force
-        }
-        Write-Host "Babashka is installed in $Directory"
-    } elseif (Test-Elevation) {
-        if ($PSVersionTable.PSVersion.Major -lt 7) {
-            [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null #redirect out to silence import
-            [System.IO.Compression.ZipFile]::ExtractToDirectory($tmpf, (Ensure-InstallDir).FullName)
-        } else {
-            Expand-Archive -Path $tmpf -DestinationPath (Ensure-InstallDir) -Force
-        }
-        Write-Host "Babashka is installed in C:\Program Files\Babashka"
-        Ensure-InstallDirOnPath
-    } else {
-        Write-Error "To install to `$env:path you need to run as admin."
+    if (Test-Path -Path $Directory -PathType Container) {
+        Get-Item -Path $Directory
+    }
+    else {
+        New-Item -Path $Directory -ItemType Directory
     }
 }
 
-DownloadLatestBabashka $Dir
+function Ensure-BabashkaProgramFileOnPath {
+    param (
+        $Directory
+    )
+    $local:OriginalEnvPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+
+    if ($OriginalEnvPath.Split(";") -contains $Directory) {
+        Write-Output "Babashka is on `$ENV:PATH."
+    }
+    else {
+        $local:DesiredEnvPath = "$OriginalEnvPath;$Directory;"
+
+        Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value  $DesiredEnvPath
+        $env:path = "$env:path;$Directory"
+        Ensure-BabashkaProgramFileOnPath -Directory $Directory
+    }
+}
+
+function RetrieveBabashka {
+    param (
+        [string]
+        $Version,
+        [string]
+        $TempFile = $(New-TemporaryFile)
+    )
+    # Determine Version url
+    if ($Version -eq "latest") {
+        $local:Releases = Invoke-WebRequest -Uri "https://api.github.com/repos/babashka/babashka/releases/$Version"
+    } else {
+        $local:Releases = Invoke-WebRequest -Uri "https://api.github.com/repos/babashka/babashka/releases/tags/$Version"
+    }
+    $local:Assets = ($Releases.Content | ConvertFrom-Json).assets # Get assets
+    $local:DownloadUrl = ($Assets | Where-Object { $_.browser_download_url -match 'windows-amd64.zip$' }).browser_download_url # Get Download URL
+
+    Invoke-WebRequest -Uri $DownloadUrl -OutFile $TempFile # Download babashka
+    Get-Item $TempFile # Return location of babashka
+}
+
+function ExpandBabashkaZip {
+    param (
+        $ZipFile
+    )
+    $TempDir = $(New-Item -Path $env:TEMP -Name "BBtmp"  -ItemType Directory -Force)
+    if ($PSVersionTable.PSVersion.Major -lt 7) {
+        [System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') > $null #redirect out to silence import
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipFile.FullName, $TempDir.FullName) > $null
+        Get-Item -Path "$env:TEMP\BBtmp\bb.exe"
+    }
+    else {
+        Expand-Archive -Path $ZipFile -DestinationPath $TempDir -Force
+        Get-Item -Path "$env:TEMP\BBtmp\bb.exe"
+    }
+}
+
+function InstallBabashka {
+    param (
+        $TargetDir,
+        $ExeLocation
+    )
+    Move-Item -Path $ExeLocation -Destination $TargetDir -Force
+    Write-Output "bb is now installed."
+}
+
+if ((Test-Elevation) -and ($Dir -eq "C:\Program Files\Babashka")) {
+    Ensure-BabashkaProgramFileOnPath -Directory $(Ensure-BabashkaProgramFile -Directory $Dir).FullName
+    $bbZip = RetrieveBabashka -Version $Version
+    $bbExe = ExpandBabashkaZip -ZipFile $bbZip
+    InstallBabashka -TargetDir $Dir -ExeLocation $bbExe
+} else {
+    $bbZip = RetrieveBabashka -Version $Version
+    $bbExe = ExpandBabashkaZip -ZipFile $bbZip
+    InstallBabashka -TargetDir $Dir -ExeLocation $bbExe
+}
